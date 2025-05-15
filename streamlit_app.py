9d6ccf6ceec3c9e19570b14d7c6e069a44076967
aiz279 <pradanafarizrizqi@gmail.com>
2025-05-15 14:20:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.image("17472911773644105829709226685962.jpg",width=200)
+st.image("17472911773644105829709226685962.jpg", width=200)
 st.write("\n")
 st.subheader("fariz sendokbesi")
 st.write("NAOOOOONNN")
